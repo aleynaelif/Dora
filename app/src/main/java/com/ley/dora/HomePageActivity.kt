56b5b9ad70106c77a20c2c59ebf57746c42505d0
Aleyna Elif ÖZKAN <51717390+aleynaelif@users.noreply.github.com>
2022-12-05 15:28:23 +0300

    request permission for gallery access

--- app/src/main/java/com/ley/dora/HomePageActivity.kt
+++ app/src/main/java/com/ley/dora/HomePageActivity.kt
@@ -1,19 +1,33 @@
 package com.ley.dora
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.ley.dora.databinding.ActivityHomePageBinding
+import java.util.jar.Manifest
 
 class HomePageActivity : AppCompatActivity() {
     private lateinit var binding : ActivityHomePageBinding
     private lateinit var auth : FirebaseAuth
+    private lateinit var activityResultLauncher : ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher :  ActivityResultLauncher <String>
+    var selectedImage : Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +36,26 @@ class HomePageActivity : AppCompatActivity() {
         setContentView(view)
 
         auth = Firebase.auth
-    }
-    fun SelectIconClicked(view : View){
 
+        registerLauncher()
+    }
+    fun SelectImageClicked(view : View){
+
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Snackbar.make(view, "Galeriye erişim izni gerekli!",Snackbar.LENGTH_INDEFINITE).setAction("İzin Ver"){
+                    //Request permission
+                    permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                }.show()
+            }else{
+                //Request permission
+                permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        }else{
+            val intentToGallery =  Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            //start activity for result
+            activityResultLauncher.launch(intentToGallery)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -43,7 +74,36 @@ class HomePageActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
-
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun registerLauncher(){
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+            if(result.resultCode == RESULT_OK){
+                val intentFromResult = result.data
+                if(intentFromResult != null){
+                    selectedImage = intentFromResult.data
+                    selectedImage?.let {
+                        binding.SelectImage.setImageURI(it)
+                    }
+                }
+            }
+        }
+
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){ result ->
+            if(result){
+                //permission granted
+                val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+            }else{
+                //permission denied
+                Toast.makeText(this@HomePageActivity,"İzin Gerekli!", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun SearchButtonClicked(view: View){
+        //image has sellected and it is in the form of uri.
+
     }
 }
